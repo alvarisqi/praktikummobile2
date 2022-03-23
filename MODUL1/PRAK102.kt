20e59f90abe33d70b85ad5f232d4d57c3f3c87e7
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-03-23 13:07:20 +0800

    Update PRAK102.kt

--- MODUL1/PRAK102.kt
+++ MODUL1/PRAK102.kt
@@ -1,9 +1,13 @@
 import java.util.*;
 
-fun main() {
+fun hitung() {
     val input = Scanner(System.`in`);
     print("Nilai x = ")
     val x = input.nextInt();
     val hasil : Double = 2*Math.pow(x.toDouble(), 2.0)+(5*x)-8;
     println("$hasil");
+}
+
+fun main() {
+    hitung();
 }
