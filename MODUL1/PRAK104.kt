528233fb02289ab59b5c10ca08dacbf642637b55
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-03-24 21:53:28 +0800

    Update PRAK104.kt

--- MODUL1/PRAK104.kt
+++ MODUL1/PRAK104.kt
@@ -3,13 +3,13 @@ package MODUL1
 class PRAK104(bidang: String, tanah: Int, penghasilan: Int) {
     val bidang: String = "Petani";
     val tanah: Int = 1500;
-    val penghasilan: Int = 10;
+    val penghasilan: Int = (4..15).random();
 }
 fun main() {
     println("Pekerjaan seorang Random");
     val bidang = "Petani";
     val tanah = 1500;
-    val penghasilan = 10;
+    val penghasilan = (4..15).random();
     val pekerjaan = PRAK104(bidang, tanah, penghasilan);
 
     println("Pak Random adalah seorang ${pekerjaan.bidang}");
