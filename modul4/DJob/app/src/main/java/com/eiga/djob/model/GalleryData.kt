a116f36e507f70d54238ac10724bf1105ffb48e1
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-05-02 21:42:00 +0800

    Delete modul4/DJob directory

--- modul4/DJob/app/src/main/java/com/eiga/djob/model/GalleryData.kt
+++
@@ -1,8 +0,0 @@
-package com.eiga.djob.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class GalleryData(
-    @DrawableRes val imageResourceId: Int
-)
--- modul4/DJob/app/src/main/java/com/eiga/djob/model/SlideshowData.kt
+++
@@ -1,9 +0,0 @@
-package com.eiga.djob.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-data class SlideshowData(
-    @StringRes val description: Int,
-    @DrawableRes val image: Int
-)
